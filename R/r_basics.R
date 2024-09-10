9e854587440f518ce80d6eeabb4cf1b6d0c0be3a
mattbagwell-ODOT <matt.t.bagwell@odot.oregon.gov>
2024-09-10 15:29:52 -0700

    made changes

--- R/r_basics.R
+++ R/r_basics.R
@@ -3,8 +3,47 @@
 #   little minds"   -Ralph Waldo Emerson 
 
 # Literals ----
+"this is a string literal" #double quotes preferred but not required
+42
+T
+FALSE
 
 # Operators ----
+2+3 #note the spacing 
+
+#Comparison
+2==2  #tests for equality
+"Joe" == "joe"
+2 == 1 + 1 #OK
+2 == (1+1) #better
+
+2!=1  #negation tests inequality
+
+2<=3
+
+
+#somewhat of a cruel joke
+TRUE == 1 #equals one
+FALSE == 0
+isTRUE(1)  #function testing if the argument is literally TRUE
+
+2<3&2>3 #both to pass criteria. 
+2<3|2>3  #OR | --pipe is for this. either/or
+2 < 3 & (2==1|2==2) #grouping statements for ordering
+
+#type matters (sometimes)
+
+"joe" # string or character type
+typeof("joe")
+42 #(double precision) numeric type floating point
+TRUE #logical or boolean type
+
+42=="42" #equality can cross types
+identical(42, "42") #type matters for identity
+
+
+
+
 
 # variables ----
 
